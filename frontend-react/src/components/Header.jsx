6698314095a1b45a0f48a26d88a0e21a584949ba
Pratik Gaikwad <gaikwadpratik8105@gmail.com>
2025-12-31 12:04:44 +0530

    Registration Page

--- frontend-react/src/components/Header.jsx
+++ frontend-react/src/components/Header.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import Button from './Button'
+import { Link } from 'react-router-dom'
 
 const header = () => {
   return (
     <>
       <nav className='navbar container pt-3 pb-3 align-items-start'>
 
-        <a className='navbar-brand text-light' href="">Stock Prediction Portal</a>
+        <Link className='navbar-brand text-light' to="/"><b>Stock Prediction Portal</b></Link>
 
         <div>
-          <Button text="Login" class = "btn-info"/>
+          <Button text="Login" class = "btn-info" url="/login" />
           &nbsp;
-          <Button text="Register" class="btn-outline-info" />
+          <Button text="Register" class="btn-outline-info" url="/register"/>
         </div>
 
       </nav>
--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -1,21 +1,16 @@
-import React from 'react'
 import Button from './Button'
+
 
 function Main() {
   return (
-    <>
+    <>  
         <div className='container'>
-
             <div className="p-5 text-center bg-light-dark rounded">
-               <h1 className='text-light'>Stock Prediction Portal</h1> 
-               <p className='text-light lead'>The Stock Prediction Portal is a smart, user-friendly platform built to empower investors with accurate, data-driven market insights. Using advanced analytics and machine learning, it predicts stock trends and highlights profitable opportunities in real time. With interactive charts, clear forecasts, and a clean dashboard, users can make faster, smarter trading decisions. Whether you’re a beginner or an experienced trader, the portal simplifies market analysis and helps you stay ahead with confidence.
-               </p>
-
-                <Button text="Login" class="btn-info" />
-
-
+              <h1 className='text-light'><b>Stock Prediction Portal</b></h1> 
+              <p className='text-light lead'>The Stock Prediction Portal is a smart, user-friendly platform built to empower investors with accurate, data-driven market insights. Using advanced analytics and machine learning, it predicts stock trends and highlights profitable opportunities in real time. With interactive charts, clear forecasts, and a clean dashboard, users can make faster, smarter trading decisions. Whether you’re a beginner or an experienced trader, the portal simplifies market analysis and helps you stay ahead with confidence.
+              </p>
+              <Button text="Login" class="btn-info" url="/login" />
             </div>
-
         </div>
     </>
   )
